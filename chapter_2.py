c09a1a4a5607966d480bf5e31e24e4b5ccce9fcf
lakpa-tamang9 <Ld.tamang25@gmail.com>
2021-11-12 18:31:06 +0900

    fourth commit

--- chapter_2.py
+++ chapter_2.py
@@ -55,3 +55,15 @@ age_0 = 22
 age_1 = 18
 
 print(age_0 >= age_1 and age_1 > 8)
+
+usernames = ['lakpa', 'sonam', 'pujan', 'pawan', 'mahesh', 'admin']
+for username in usernames:
+    if username == 'admin':
+        print(f'Hello {username}.title(), would you like to see a status report?')
+    else:
+        print(f'Hello {username}.title(), thank you for logging again.')
+
+users = []
+for user in users:
+    if user == '':
+        print('We need to find some users')
