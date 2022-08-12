b0fba2e37bc1446ddb7d34e0de13216675bd1771
lakpa-tamang9 <Ld.tamang25@gmail.com>
2022-08-12 15:41:13 +0900

    add polylmorphism examples

--- inheritance.py
+++ inheritance.py
@@ -2,9 +2,11 @@
 # Parent class is the super class that serves as a fundamental class
 # child classes are inside the parent class and they share similar property with the parent class.
 
+# Polymorphism:  It refers to the use of a single type entity (method, operator or object)
+# to represent different types in different scenarios.
 
-from turtle import Turtle
-from unicodedata import name
+# We can use the concept of polymorphism while creating class methods as
+# Python allows different classes to have methods with the same name.
 
 
 class ProgrammingLanguages:
@@ -31,20 +33,27 @@ class Python(ProgrammingLanguages):
         super().__init__(name)
         # super init inherits the __init__ method from the parent class.
 
+    def popular(self):
+        return "This is popular among data scientists, automation, Machine learning "
+
 
 class Javascript(ProgrammingLanguages):
     def __init__(self, name):
         super().__init__(name)
 
-    def popular(self, purpose):
-        return super().popular(purpose)
+    def popular(self):
+        return "This is very popular along with HTML and CSS for website development"
 
+
+# COncept of polymorphism: In the above classes, popular method is inherited from the parent class
+# and used in two child classes with different purposes. ALthough they have same name,
+# they serve differnt purpose and can be modified accordingly.
 
 python = Python("Python")
-print(python.application())
-
+# print(python.application())
+print(python.popular())
 javascript = Javascript("Javascript")
-print(javascript.application())
+# print(javascript.application())
 
-print(javascript.complexity(complex=True))
-print(javascript.popular(purpose="Web development"))
+# print(javascript.complexity(complex=True))
+# print(javascript.popular(purpose="Web development"))
