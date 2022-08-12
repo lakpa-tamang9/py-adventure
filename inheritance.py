27acafc63c2f280df9376d27c33b3181f1bf2652
lakpa-tamang9 <Ld.tamang25@gmail.com>
2022-08-12 15:26:28 +0900

    update example

--- inheritance.py
+++ inheritance.py
@@ -3,6 +3,7 @@
 # child classes are inside the parent class and they share similar property with the parent class.
 
 
+from turtle import Turtle
 from unicodedata import name
 
 
@@ -15,6 +16,15 @@ class ProgrammingLanguages:
             f"{self.name} Programming language are used to build softwares and automation"
         )
 
+    def complexity(self, complex):
+        if complex:
+            print(f"{self.name} is complex to learn")
+        else:
+            print(f"{self.name} is easy to learn")
+
+    def popular(self, purpose):
+        print(f"{self.name} is popular for {purpose}")
+
 
 class Python(ProgrammingLanguages):
     def __init__(self, name):
@@ -26,9 +36,15 @@ class Javascript(ProgrammingLanguages):
     def __init__(self, name):
         super().__init__(name)
 
+    def popular(self, purpose):
+        return super().popular(purpose)
+
 
 python = Python("Python")
 print(python.application())
 
 javascript = Javascript("Javascript")
 print(javascript.application())
+
+print(javascript.complexity(complex=True))
+print(javascript.popular(purpose="Web development"))
